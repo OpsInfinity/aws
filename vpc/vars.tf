6c36fa15841bb9a22ca73c7d2b9891a581dc4d9f
Chowdary-Hari <chandrahari321@outlook.com>
2025-04-18 11:43:47 +0530

    Automated commit #1208

--- vpc/vars.tf
+++ vpc/vars.tf
@@ -7,6 +7,4 @@ variable "private_subnet_cidrs" {}
 variable "database_subnet_cidrs" {}
 variable "account_no" {}
 variable "default_vpc_id" {}
-variable "default_route_table_id" {
-
-}
+variable "default_route_table_id" {}
