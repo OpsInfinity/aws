8b1ed8ce819089042749042a8cabfb295cf40893
Chowdary-Hari <chandrahari321@outlook.com>
2025-04-20 21:16:22 +0530

    Automated commit #1219

--- vpc/output.tf
+++ vpc/output.tf
@@ -10,6 +10,6 @@ output "database_subnet_ids" {
   value = aws_subnet.database_subnets[*].id
 }
 
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
+# output "vpc_id" {
+#   value = aws_vpc.main.id
+# }
